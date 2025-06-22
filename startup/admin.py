3554ac6e0e03a0a9ca45c6752fdf5684f4a7ed18
junaid089 <junu089@gmail.com>
2025-06-22 15:49:29 +0530

    initial commit

---
+++ startup/admin.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.decorators import login_required
+
+from pr1 import admin
+
+
+@login_required
+class StarterAdmin(admin.ModelAdmin):
+    list_display = ('email', 'role', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email',)
+
+

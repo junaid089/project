85a264f268e438941be0ff6fa5fa1f759e4eebd5
junaid089 <junu089@gmail.com>
2025-06-23 22:35:55 +0530

    initial commit

--- startup/models.py
+++ startup/models.py
@@ -5,3 +5,9 @@ class Starter(models.Model):
     email = models.EmailField()
     def __str__(self):
         return self.name
+
+class second(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    def __str__(self):
+        return self.name

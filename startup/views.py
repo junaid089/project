54db1c1e4abcee23b5bdea16c1bcb251b55bf880
junaid089 <junu089@gmail.com>
2025-06-23 22:57:31 +0530

    initial commit

--- startup/views.py
+++ startup/views.py
@@ -6,6 +6,7 @@ def index(request):
     if request.user.is_authenticated:
         return render(request, 'index.html')
     return render(request, 'index.html')
+
 def login(request):
     if request.user.is_authenticated:
         return render(request, 'login.html')

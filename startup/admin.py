e6478e12e654b21da6d4889804be67832b592665
junaid089 <junu089@gmail.com>
2025-06-23 22:39:26 +0530

    initial commit

--- startup/admin.py
+++ startup/admin.py
@@ -10,3 +10,7 @@ class StarterAdmin(admin.ModelAdmin):
     search_fields = ('email',)
 
 
+class SecondAdmin(admin.ModelAdmin):
+    list_display = ('email', 'role', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email',)

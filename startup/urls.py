3ead49205c7b56a170821ffa741dc795e6e806b2
junaid089 <junu089@gmail.com>
2025-06-23 22:54:31 +0530

    initial commit

--- startup/urls.py
+++ startup/urls.py
@@ -3,8 +3,10 @@ import namespace
 from pr1 import admin
 from project.pr1.urls import urlpatterns
 from startup.admin import SecondAdmin
+from startup.models import Starter
 
 urlpatterns=urlpatterns+[
 
     ('admin/', admin.site.urls),
+    ('login/', Starter)
 ]

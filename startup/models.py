9c3aba400e039a7b714895f3be7be2de603bd518
junaid089 <junu089@gmail.com>
2025-06-23 22:40:41 +0530

    initial commit

--- startup/models.py
+++ startup/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Starter(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
+
     def __str__(self):
         return self.name
 

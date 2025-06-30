752e119200f46b167f73568f965f597d8293ff26
junaid089 <junu089@gmail.com>
2025-06-30 23:52:35 +0530

    initial commit

--- startup/admin.py
+++ startup/admin.py
@@ -17,4 +17,4 @@ class SecondAdmin(admin.ModelAdmin):
 
 class newAdmin(admin.ModelAdmin):
     list_display = ('email', 'phone', 'role', 'is_staff', 'is_active')
-    list_display
+    list_display = ('email', 'phone', 'role', 'is_staff', 'is_active')

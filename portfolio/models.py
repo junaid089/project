78474474e0928c11e22861686c6cc3507adf792c
junaid089 <junu089@gmail.com>
2025-08-20 00:23:14 +0530

    initial commit

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Skill(models.Model):
     name = models.CharField(max_length=100)
-    proficiency = models.IntegerField()  # Percenta
+    proficiency = models.IntegerField()  # Percentage
 
     def __str__(self):
         return self.name
